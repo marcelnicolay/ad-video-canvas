411c55c8b4ac6fc5f227cf261126d652d4bfed53
Rodrigo Machado <rcmachado@gmail.com>
2011-11-26 11:01:11 -0200

    heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: ./bin/gunicorn -k tornado --workers=1 --bind=0.0.0.0:$PORT advideo.start
+web: ./bin/gunicorn -k tornado --workers=1 --bind=0.0.0.0:$PORT advideo.start_heroku
--- advideo/start_heroku.py
+++ advideo/start_heroku.py
@@ -1,14 +1,15 @@
 # coding: utf-8
-#!/usr/bin/env python
 
 import os, sys, atexit, getopt
 import tornado.web
+import tornado.wsgi
 import tornado.options
 import tornado.ioloop
 import logging
+import wsgiref.simple_server
 
 def main():
-    
+
     tornado.options.define("BUSCAPE_API_ID", default="6a71545654426b56542b453d", help="")
     tornado.options.define("BUSCAPE_API_URL", default="http://bws.buscape.com/service/%(service)s/%(application_id)s/", help="")
     tornado.options.define("FONTS_DIR", default="%s/static/fonts/" % os.path.abspath(os.path.dirname(__file__)))
@@ -21,21 +22,19 @@ def main():
     # settings, path.insert
     project_root = os.path.abspath(os.path.dirname(__file__))
     sys.path.insert(0, os.path.abspath("%s/.." % project_root))
-    
+
     logging.getLogger().setLevel(getattr(logging, tornado.options.options.logging.upper())) 
-    
+
     from advideo.handler import FindAdHandler, ImgProcHandler
 
-    application = tornado.web.Application([
+    application = tornado.wsgi.WSGIApplication([
         (r"/", ImgProcHandler),
         (r"/findad", FindAdHandler),
         (r"/static/(.*)", tornado.web.StaticFileHandler, {"path": os.path.join(os.path.dirname(__file__), "static")}),
     ])
-    
-    application.listen(8888)
-    tornado.ioloop.IOLoop.instance().start()
-        
+
+    server = wsgiref.simple_server.make_server('', 8888, application)
+    server.serve_forever()
+
 if __name__ == "__main__":
     main()
-
-
